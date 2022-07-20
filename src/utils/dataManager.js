163307bb310c9a877b5893b51d16e6dfdeff3f0a
Cuauhtemoc Ramirez Hernandez <cramhd@gmail.com>
2022-07-20 09:54:26 -0500

    fix tableland connection

--- src/utils/dataManager.js
+++ src/utils/dataManager.js
@@ -4,34 +4,36 @@ export default {
   async createUserDataStorage() {
     // Connect to the Tableland testnet (defaults to Goerli testnet)
     // @return {Connection} Interface to access the Tableland network and target chain
-    const tableland = await connect({ network: "testnet" });
+    // TODO: move the connection params to config parameters
+    const tableland = await connect({ network: "testnet", chain: "polygon-mumbai" });
 
     // we look for exisiting table to avoid re-create a new one
-    // TODO: make the table id a config parameter``
-    const hashRes = await tableland.hash('CREATE TABLE AssetTracker_5 (id INT, file_name TEXT, file_type TEXT, ipfs_cid TEXT, provider TEXT, ipfs_gateway TEXT, primary key (id));')
+    // TODO: make the chain id a config parameter
+    const hashRes = await tableland.hash('CREATE TABLE AssetTracker_80001 (id INT, file_name TEXT, file_type TEXT, ipfs_cid TEXT, provider TEXT, ipfs_gateway TEXT, primary key (id));')
     const appTableStructure = hashRes.structureHash;
 
     const tables = await tableland.list(); // returns an Object with the Tables the connected address owns
     console.log(">> all tables: ", tables)
-    const myMatchingTables = Object.values(tables).filter((table) => table.structure === appTableStructure) // filters `tables` for matching `structure`s
-    console.log(">> myMatchingTables: ", myMatchingTables)
+    const existingTables = Object.values(tables).filter((table) => table.structure === appTableStructure) // filters `tables` for matching `structure`s
+    console.log(">> existingTables: ", existingTables)
 
     // if there is no table created already we create a new one
-    if (myMatchingTables.length == 0) {
+    if (existingTables.length == 0) {
       // Create a new table with a supplied SQL schema and optional `prefix`
       // @return {Connection} Connection object, including the table's `name`
       const { name } = await tableland.create(`id INT, file_name TEXT, file_type TEXT, ipfs_cid TEXT, provider TEXT, ipfs_gateway TEXT, primary key (id)`, 'AssetTracker');
       
-      //const table = await tableland.create(`id INT, file_name TEXT, file_type TEXT, ipfs_cid TEXT, provider TEXT, ipfs_gateway TEXT, primary key (id)`, 'AssetTracker');
-      //console.log(">> table: ", table)
-
       // The table's `name` is in the format `{prefix}_{chainId}_{tableId}`
-      //console.log("new table name: ", name);
+      console.log("new table name: ", name);
       
       window.assetTrackerTable = name;
-      window.tableland = tableland;
+    } else {
+
+      window.assetTrackerTable = existingTables[0].name;
     }
 
+    console.log(">> window.assetTrackerTable: ", window.assetTrackerTable);
+    window.tableland = tableland;
     return tableland;
   },
 
