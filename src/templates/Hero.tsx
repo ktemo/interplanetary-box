a45b50f07ccc25f5ec75fc66e8dd3fc466afd00c
“seeinplays” <seeinplays@gmail.com>
2022-07-22 11:34:03 -0600

    have different view when logged in with metamask

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,31 +1,17 @@
-import { Background } from '../background/Background';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
-import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
-import { ConnectMetamaskButtonComponent } from './ConnectWallet';
-import { Logo } from './Logo';
 
 const Hero = () => (
-  <Background color="bg-gray-100">
-    <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <ConnectMetamaskButtonComponent />
-        </li>
-      </NavbarTwoColumns>
-    </Section>
-
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            <span className="text-primary-500">Interplanetary Box</span>
-          </>
-        }
-        description="A decentralized service to upload and share content with anyone"
-      />
-    </Section>
-  </Background>
+  <Section yPadding="pt-20 pb-32">
+    <HeroOneButton
+      title={
+        <>
+          <span className="text-primary-500">Interplanetary Box</span>
+        </>
+      }
+      description="A decentralized service to upload and share content with anyone"
+    />
+  </Section>
 );
 
 export { Hero };
